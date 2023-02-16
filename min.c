4557810dfb8c20226046372a12e341035b9249cd
wl_git <2093328074@qq.com>
2023-02-16 10:12:26 +0800

    新加了play函数

--- min.c
+++ min.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #define Min ((a) < (B)?(a):(b))
 
+void play()
+{
+	printf("happy new year!");
+} 
 
 int main(void)
 {
     int  a = 50, b = 100;
     printf("%d\n",Min(a,b));
+    play();
     return 0;
-
 }
 
 
